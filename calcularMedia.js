b7a2d8c5cf91f8c6c9bd0b2acba1a3ebb41fb5da
thalssn <109880284+thalssn@users.noreply.github.com>
2024-09-12 19:06:40 -0300

    else if para o caso de recuperação

--- calcularMedia.js
+++ calcularMedia.js
@@ -1,10 +1,12 @@
-let n1, n2, qtdFaltas, media, calculo, aprovado;
+let n1, n2, qtdFaltas, media, calculo, aprovado, recuperacao;
+
     
 n1 = parseFloat(prompt("Digitar n1"));
 n2 = parseFloat(prompt("Digitar n2"));
 qtdFaltas = prompt("Digite a quantidade de faltas: ")
 
 media = (n1+n2)/2;
+recuperacao = false;
 
 //console.log(media);
 
@@ -14,19 +16,23 @@ console.log(calculo);
 console.log(typeof(calculo));
 
 if (media >=7 && qtdFaltas <=10){
-aprovado = true;
-//console.log(aprovado) exibir no console conteúdo da variável
-//console.log(typeof(aprovado)) exibir no console tipo da variável
+    aprovado = true;
+    //console.log(aprovado) exibir no console conteúdo da variável
+    //console.log(typeof(aprovado)) exibir no console tipo da variável
+}else if(media>=3 && media < 7 && qtdFaltas <= 10){
+    recuperacao = true;
+    // console.log(aprovado) exibir no console conteúdo da variável
+    // console.log(typeof(aprovado)) exibir no console tipo da variável
 }else{
-aprovado = false;
-// console.log(aprovado) exibir no console conteúdo da variável
-// console.log(typeof(aprovado)) exibir no console tipo da variável
+    aprovado = false;
 }
 
 if (aprovado === true){
-alert("Aprovado")
+    alert("Aprovado")
+}else if(recuperacao === true){
+    alert("Em recuperação")
 }else{
-alert("Reprovado")
+    alert("Reprovado")
 }
 
 //testando negação
